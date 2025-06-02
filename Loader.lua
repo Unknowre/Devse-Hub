f694a59dd57ba21592ba55c4474f21a9a549ef48
UNKNOW <58127457+Unknowre@users.noreply.github.com>
2025-06-02 22:01:12 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,6 +3,7 @@ local scripts = {
     [17493355683] = "FRG.lua",
     [17493357698] = "FRG.lua",
     [76381016848158] = "FR.lua",
+    [75794995434025] = "GAB.lua",
 }
 
 local placeId = game.PlaceId
