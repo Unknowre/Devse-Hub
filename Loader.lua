622d336cba4ff0749e1304526db61b347a3df1bc
UNKNOW <58127457+Unknowre@users.noreply.github.com>
2025-06-14 19:30:20 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,16 +1,16 @@
-local scripts = {
-    [9224601490] = "FRG.lua",
-    [17493355683] = "FRG.lua",
-    [17493357698] = "FRG.lua",
-    [76381016848158] = "FR.lua",
-    [75794995434025] = "GAB.lua",
+local luarmorLoaders = {
+    [9224601490] = "84b14e6e92ce43e095bc7927aec8e989",
+    [17493355683] = "84b14e6e92ce43e095bc7927aec8e989",
+    [17493357698] = "84b14e6e92ce43e095bc7927aec8e989",
+    [76381016848158] = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    [75794995434025] = "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy",
 }
 
 local placeId = game.PlaceId
-local scriptName = scripts[placeId]
+local loaderId = luarmorLoaders[placeId]
 
-if scriptName then
-    local url = "https://raw.githubusercontent.com/Unknowre/Devse-Hub/main/" .. scriptName
+if loaderId then
+    local url = "https://api.luarmor.net/files/v3/loaders/" .. loaderId .. ".lua"
     loadstring(game:HttpGet(url))()
 else
     game:GetService("StarterGui"):SetCore("SendNotification", {
