be01459455fd35a42dbc89b4674b61a07a606706
UNKNOW <58127457+Unknowre@users.noreply.github.com>
2025-07-14 01:49:35 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,20 +1,22 @@
 local scripts = {
-    [9224601490] = "FRG.lua",
-    [17493355683] = "FRG.lua",
-    [17493357698] = "FRG.lua",
-    [12413901502] = "FRG.lua",
-    [16190471004] = "FRG.lua",
-    [76381016848158] = "FR.lua",
-    [75794995434025] = "GAB.lua",
-    [75663528075786] = "DRAGON.lua",
-    [73727760725654] = "STC.lua",
-    [126884695634066] = "GAG.lua",
+    [9224601490] = "Rk5HlnVhLnB5",
+    [17493355683] = "Rk5HlnVhLnB5",
+    [17493357698] = "Rk5HlnVhLnB5",
+    [12413901502] = "Rk5HlnVhLnB5",
+    [16190471004] = "Rk5HlnVhLnB5",
+    [76381016848158] = "RlIubG",
+    [75794995434025] = "R0FCLnB5",
+    [75663528075786] = "RFJBR09OLnB5",
+    [73727760725654] = "U1RDLnB5",
+    [126884695634066] = "R0FHLnB5",
 }
 
 local placeId = game.PlaceId
-local scriptName = scripts[placeId]
+local base64ScriptName = scripts[placeId]
 
-if scriptName then
+if base64ScriptName then
+    local scriptName = game:GetService("HttpService"):Base64Decode(base64ScriptName)
+    
     local url = "https://raw.githubusercontent.com/Unknowre/Devse-Hub/main/" .. scriptName
     loadstring(game:HttpGet(url))()
 else
